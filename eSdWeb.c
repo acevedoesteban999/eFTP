3b49e96435eaac9ffe316a8b7b3ef2aa70ebd3dc
Esteban <acevedoesteban999@gmail.com>
2025-01-17 11:04:00 -0500

    .

--- eSdWeb.c
+++ eSdWeb.c
@@ -28,19 +28,9 @@ void list_files() {
 
 
 esp_err_t esdweb_handler(httpd_req_t *req){
-    //DO SOMETHING
-    static_ctx_handler *ctx = (static_ctx_handler *)req->user_ctx;
-    if ( ctx && ctx->uri_execution_function) {
-        if(!ctx->uri_execution_function(req)){
-            httpd_resp_set_status(req, "400 Bad Request");
-            httpd_resp_set_hdr(req, "Content-Type", "text");
-            httpd_resp_send(req, "No conditional function pass", HTTPD_RESP_USE_STRLEN);
-            return ESP_OK;
-        } 
-    } 
-    list_files();
+    // list_files();
     char*buffer;
-    EWEB_GENERATE_REPLACEMENT_BUFFER(buffer,sdweb_html_asm_start,ad);
+    EWEB_GENERATE_REPLACEMENT_BUFFER(buffer,sdweb_html_asm_start,"Hola Mundo");
     httpd_resp_set_type(req, "text/html");
     esp_err_t err = eweb_send_resp_try_chunk(req,buffer,strlen(buffer));
     free(buffer);
