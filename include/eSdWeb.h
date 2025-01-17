dcb0cf6f1dfd0d75a035c03a31ec211ae030e615
Esteban <acevedoesteban999@gmail.com>
2025-01-17 11:33:52 -0500

    ADD: Adapt Minify HTML, CSS , JS

--- include/eSdWeb.h
+++ include/eSdWeb.h
@@ -4,12 +4,12 @@
 
 
 #define ESDWEB_HANDLERS( execution_funtion ,handler_html,handler_statics) \
-    {{"/sd.html", HTTP_GET , execution_funtion , NULL}, true, {sdweb_html_asm_start,sdweb_html_asm_end,"",esdweb_handler,NULL}}
+    {{"/sd.min.html", HTTP_GET , execution_funtion , NULL}, true, {sdweb_html_asm_start,sdweb_html_asm_end,"",esdweb_handler,NULL}}
 
 
 
-/* {{"/css/ota.css", HTTP_GET , handler_statics , NULL}, true, {ota_css_asm_start,ota_css_asm_end,"text/css",NULL}}, \
-// {{"/js/ota.js", HTTP_GET , handler_statics , NULL}, true, {ota_js_asm_start,ota_js_asm_end,"text/javascript",NULL}}, \
+/* {{"/css/ota.min.css", HTTP_GET , handler_statics , NULL}, true, {ota_css_asm_start,ota_css_asm_end,"text/css",NULL}}, \
+// {{"/js/ota.min.js", HTTP_GET , handler_statics , NULL}, true, {ota_js_asm_start,ota_js_asm_end,"text/javascript",NULL}}, \
  {{"/ota_update", HTTP_POST, concidional_funtion, NULL}, true, {.uri_execution_function = ota_post_handler}}, \
 */
 
